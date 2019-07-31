112ca7b9183fa0ae079d1255d507afa4fb30c0b0
Alin <15910344021@163.com>
2019-07-31 21:59:14 +0800

    SimpleControllerHandlerAdapter

---
+++ src/main/java/cn/zl/entity/Person.java
@@ -0,0 +1,17 @@
+package cn.zl.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author Alin
+ * @desc //
+ * @date 2019/7/31
+ */
+@ToString
+@Setter
+@Getter
+public class Person {
+    private String name;
+}
